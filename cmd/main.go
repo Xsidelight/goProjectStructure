f47ba3506e84709397ed032d1ad6e27bbc93204a
Xsidelight <toko.gogberaa@gmail.com>
2023-12-19 20:05:49 +0600

    Add routes and start up for main

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main()  {
-	
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Xsidelight/goProjectStructure/internal/routes"
+)
+
+func main() {
+	router := routes.NewRouter()
+
+	port := 8080
+	addrs := fmt.Sprintf("%d", port)
+
+	fmt.Printf("Listening on port %s\n", addrs)
+	err := http.ListenAndServe(":"+addrs, router)
+	if err != nil {
+		panic(err)
+	}
+
 }
